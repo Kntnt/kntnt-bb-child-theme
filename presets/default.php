5d2d39da3837ff9d9e9df02b012debc777a88a40
Thomas Barregren <thomas@kntnt.com>
2017-12-29 18:50:09 +0100

    Many small improvements.

---
+++ presets/default.php
@@ -0,0 +1,56 @@
+<?php
+ 
+return [
+
+  // Display name in Customizer
+  'name' => "Default",
+
+  // To load a LESS file named skin-NAME.less in the less directory, set
+  // skin => 'NAME'. Leave empty to not load additional skin files.
+  'skin' => '',
+
+  // Default values in Customizer settings.
+  'settings' => [
+
+    // Viewing area's background color
+    'fl-body-bg-color' => '#eeeeee',
+
+    // Content layout
+    'fl-layout-width' => 'full-width',
+    'fl-content-width' => '945',
+
+    // Body font
+    'fl-body-font-family' => 'Lora',
+    'fl-body-font-size' => '24',
+    'fl-body-font-weight' => '400',
+    'fl-body-line-height' => '1.6',
+
+    // Body color
+    'fl-body-text-color' => '#333333',
+    'fl-content-bg-color' => '#fdfdfd',
+
+    // Heading font
+    'fl-heading-font-family' => 'Arimo',
+    'fl-heading-font-weight' => '700',
+    'fl-h1-font-size' => '48',
+    'fl-h1-line-height' => '1.4',
+    'fl-h2-font-size' => '36',
+    'fl-h2-line-height' => '1.4',
+    'fl-h3-line-height' => '1.4',
+    'fl-h4-font-size' => '24',
+    'fl-h4-line-height' => '1.4',
+    'fl-h5-font-size' => '24',
+    'fl-h5-line-height' => '1.6',
+    'fl-h6-font-size' => '24',
+    'fl-h6-line-height' => '1.6',
+
+    // Heading color
+    'fl-heading-text-color' => '#333333',
+
+    // Accent color
+    'fl-accent' => '#119a4e',
+    'fl-accent-hover' => '#6ec4d4',
+
+  ],
+
+];
