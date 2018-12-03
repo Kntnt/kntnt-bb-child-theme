c52ec495f45ce2f34d9abd67467703f2301ed96f
Thomas Barregren <thomas@kntnt.se>
2018-12-03 11:25:59 +0100

    Added back workaround for paginaion error due to recurring bug. Bug report sent to BB on Dcember 2018

--- classes/class-bb-builder-enhancer.php
+++ classes/class-bb-builder-enhancer.php
@@ -6,6 +6,7 @@ class BB_Builder_Enhancer {
 
 	public function run() {
 		add_filter( 'fl_theme_compile_less_paths', [ $this, 'set_less_paths' ], 13 );
+		add_filter( 'rewrite_rules_array', [ $this, 'pagination_rewrite_rules' ] );
 	}
 
 	/**
@@ -14,6 +15,19 @@ class BB_Builder_Enhancer {
 	public function set_less_paths( $paths ) {
 		$paths[] = THEME_DIR . '/less/bb-page-builder.less';
 		return $paths;
+	}
+
+ 	/**
+	 * Beaver Builder add rewrite rules for pagination. These handle simpler
+	 * permalink structures, including the predefined, but not more complex like
+	 * %postname%/%post_id%. A bug report was filed 21 September and 3 December
+	 * 2018. Awaiting a fix, this filter adds support for the format
+	 * %postname%/%post_id%.
+	 */
+	public function pagination_rewrite_rules( $rules ) {
+		$paged_regex = 'paged-[0-9]{1,}'; // See FLBuilderLoop::$paged_regex_base
+		$new_rules[".+/(\d+)/$paged_regex/(\d+)"] = 'index.php?p=$matches[1]&flpaged=$matches[2]'; // See FLBuilderLoop::init_rewrite_rules
+		return $new_rules + $rules;
 	}
 
 }
